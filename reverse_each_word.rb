362f2f9f00a69a76ebed22a65777c883ef4d2805
Chely Ho <adept_inept@yahoo.com>
2018-02-07 01:50:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(words)
-  #words.reverse.split.reverse.join(" ")               # reverse whole sentence, split into array, reverse order of each, and joins back up
+  #words.reverse.split.reverse.join(" ")               # reverse whole sentence, split into array, reverse order of each, and joins back up. can add method upon methods
 
   words.split.collect{|word| word.reverse}.join(" ")   # looks a little grody. split into array, reverses each word, and joins back up. learned that you can add methods to blocks. https://stackoverflow.com/questions/19118688/ruby-array-map-and-join-in-one-loop
 end
