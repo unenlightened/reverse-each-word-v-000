be2ba7ba819471e39a8b3a2dd77aeb32b0363021
Chely Ho <adept_inept@yahoo.com>
2018-02-07 01:29:37 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,3 @@
-def reverse_each_word(string)
-  words = string.split(" ")
-  reversed_words = []
-
-  words.collect do |word|
-    reversed_words << word.reverse
-  end
-
-  reversed_words.join(" ")
+def reverse_each_word(words)
+  words
 end
